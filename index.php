ebd855790440faac0069e198eb8617d27e075907
Bastoune7 <bastienkulmatiski@gmail.com>
2024-02-22 14:48:18 +0100

    ✨Refonte Arborescence 🪐
    Refonte totale de l'arborescence du projet.

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <?php
+include 'includes/header.php';
+?>
 
-// Redirection vers /public
+<!-- Contenu de la page d'accueil -->
+<section id="home">
+    <div class="container">
+        <h1>Bienvenue sur mon portfolio</h1>
+        <p>Découvrez mes projets et compétences en développement web.</p>
+    </div>
+</section>
 
-if (strpos($_SERVER['REQUEST_URI'], '/portfolio/public') === false) {
-    header('Location: /portfolio/public');
-    exit();
-}
+
+<!-- Autres sections ou contenus spécifiques à la page d'accueil -->
+
+<?php
+//inclusion du fichier de pied de page
+include 'includes/footer.php';
+?>
--- templates/index.php
+++
@@ -1,9 +0,0 @@
-<!-- templates/index.php -->
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Mon Portoflio</title>
-    <link rel="stylesheet" href="../assets/style.css">
-</head>
-</html>
